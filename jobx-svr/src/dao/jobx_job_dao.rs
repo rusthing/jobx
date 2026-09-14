b59f2aae25469b862d603b87444e998abc76f89a
nnzbz <nnzbz@qq.com>
2026-09-14 21:00:50 +0800

    * 优化查询计划

--- jobx-svr/src/dao/jobx_job_dao.rs
+++ jobx-svr/src/dao/jobx_job_dao.rs
@@ -1,6 +1,9 @@
 use jobx_api::dic::JobType;
+use jobx_api::mo::jobx_task;
 use robotech::macros::dao;
+use sea_orm::sea_query::{BinOper, Expr, Query, SimpleExpr};
 use sea_orm::ColumnTrait;
+use sea_orm::ExprTrait;
 
 /// 任务计划
 #[dao(
@@ -21,11 +24,27 @@ impl JobxJobDao {
     where
         C: ConnectionTrait,
     {
+        let not_exists = Query::select()
+            .expr(Expr::value(1))
+            .from(jobx_task::Entity)
+            .and_where(Expr::binary(
+                Expr::col(jobx_task::Column::JobId),
+                BinOper::Equal,
+                Expr::col(Column::Id),
+            ))
+            .and_where(Expr::binary(
+                Expr::col(jobx_task::Column::ScheduledAssignTs),
+                BinOper::Equal,
+                Expr::col(Column::NextAssignTs),
+            ))
+            .to_owned();
+
         Entity::find()
             .filter(Column::Enabled.eq(true))
             .filter(Column::JobType.ne(JobType::Manual.value()))
             .filter(Column::NextAssignTs.gte(begin))
             .filter(Column::NextAssignTs.lte(end))
+            .filter(SimpleExpr::from(Expr::exists(not_exists).not()))
             .all(db)
             .await
             .map_err(|e| DaoError::parse_db_err(e))
