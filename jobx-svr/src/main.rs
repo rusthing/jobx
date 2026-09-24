9871242ef1df7ce554098e6dd679dfe7ad6136ab
nnzbz <nnzbz@qq.com>
2026-09-24 14:33:39 +0800

    * 优化代码

--- jobx-svr/src/main.rs
+++ jobx-svr/src/main.rs
@@ -23,12 +23,12 @@ async fn setup(
     let db_url = app_config.db.get_url();
     db_migrate!(db_url);
 
-    setup_id_worker(app_config.id_worker.clone(), &changed)?;
-    setup_redis_conn(app_config.redis.clone(), &None).await?;
-    setup_db_conn(app_config.db.clone(), &changed).await?;
+    setup_id_worker(app_config.id_worker.clone(), changed)?;
+    setup_redis_conn(app_config.redis.clone(), changed).await?;
+    setup_db_conn(app_config.db.clone(), changed).await?;
 
-    setup_jobx_scheduler(app_config.jobx_scheduler.clone(), &changed);
-    setup_web_server(app_config.web.clone(), port, old_pid, &changed).await?;
+    setup_jobx_scheduler(app_config.jobx_scheduler.clone(), changed);
+    setup_web_server(app_config.web.clone(), port, old_pid, changed).await?;
 
     Ok(())
 }
